573e4b97dad4371658bc8e8cb1339c2900dbe93d
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2024-01-31 04:19:10 +0100

    feat: Add BarometerInfo widget to display barometer information

--- lib/ui/widgets/main_screen/main_screen_widget.dart
+++ lib/ui/widgets/main_screen/main_screen_widget.dart
@@ -60,10 +60,73 @@ class ViewWidget extends StatelessWidget {
           ),
           const SizedBox(height: 16),
           ForecastHours(state: weatherState),
-          WindInformation(weatherState: weatherState)
+          WindInformation(weatherState: weatherState),
+          const SizedBox(height: 16),
+          BarometerInfo(weatherState: weatherState),
         ],
       ),
     );
+  }
+}
+
+class BarometerInfo extends StatelessWidget {
+  const BarometerInfo({
+    super.key,
+    required this.weatherState,
+  });
+
+  final WeatherState weatherState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            appText(
+              size: 24,
+              text: "Barometer",
+              isBold: FontWeight.bold,
+            ),
+            Column(children: [
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Icon(Icons.thermostat_outlined, color: primaryColor),
+                  appText(
+                    size: 18,
+                    text:
+                        'Temperature: ${weatherState.forecastObject?.current?.tempC} °C',
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Icon(Icons.opacity, color: primaryColor),
+                  appText(
+                    size: 18,
+                    text:
+                        'Humidity: ${weatherState.forecastObject?.current?.humidity} °%',
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Icon(Icons.speed, color: primaryColor),
+                  appText(
+                    size: 18,
+                    text:
+                        'Pressure: ${weatherState.forecastObject?.current?.pressureMb} hPa',
+                  ),
+                ],
+              ),
+            ])
+          ],
+        ));
   }
 }
 
@@ -95,8 +158,7 @@ class WindInformation extends StatelessWidget {
               const Icon(Icons.wb_sunny, color: primaryColor),
               appText(
                 size: 18,
-                text:
-                    '${weatherState.forecastObject?.current?.windKph} km/h',
+                text: '${weatherState.forecastObject?.current?.windKph} km/h',
               ),
               appText(
                 size: 18,
